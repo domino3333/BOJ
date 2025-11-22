acaf1747c2ecdda729575f7c57a5b0e80c806a7f
miin9999 <busster@naver.com>
2025-11-22 23:57:58 +0900

    알파벳개수

---
+++ src/bronze_silver/q_10808.java
@@ -0,0 +1,26 @@
+package bronze_silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q_10808 {
+
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s= br.readLine();
+
+		int[]arr = new int[26];
+
+		for(int i =0;i<s.length();i++) {
+			char c = s.charAt(i);
+			arr[c - 97]++;
+		}
+		 for (int i = 0; i < 26; i++) {
+	            System.out.print(arr[i] + " ");
+	        }
+
+	}
+
+}
